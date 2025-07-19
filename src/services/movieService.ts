151d13f70a7ad7f3bc87bc034d5240728ddc7951
SotnikovSS <sotnikovss90@gmail.com>
2025-07-19 12:54:42 +0200

    updt

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -8,21 +8,19 @@ interface MoviesHttpResponse {
 
 const myKey = import.meta.env.VITE_API_KEY;
 
-export const getMovie = async (query: string, page: number): Promise<MoviesHttpResponse> => {
-
+export const getMovie = async (
+  query: string,
+  page: number
+): Promise<MoviesHttpResponse> => {
   const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=${page}`;
   const options = {
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization:
-        `Bearer ${myKey}`,
+      Authorization: `Bearer ${myKey}`,
     },
   };
 
   const response = await axios.get<MoviesHttpResponse>(url, options);
-  console.log(response.data.total_pages)
   return response.data;
 };
-
-
